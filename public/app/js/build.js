2b6e23b57f611b6bba81aeda25dc85a5a805e5ae
kellyh6 <faithkeeping@yahoo.com>
2017-03-30 16:16:11 -0700

    custom card builder update

--- public/app/js/build.js
+++ public/app/js/build.js
@@ -6,33 +6,51 @@ angular.module('BuildCtrls', [])
     $scope.backBlack = false;
     $scope.frontWhite = true;
     $scope.backWhite = false;
+    $scope.customBlack = {
+    	question: '',
+    	blanks: 0
+    };
+    $scope.customWhite = {
+    	answer: ''
+    };
 
     $scope.togglefrontBlack = function() {
         $scope.frontBlack = !$scope.frontBlack
         $scope.backBlack = false;
       }
-      $scope.togglebackBlack = function() {
+    $scope.togglebackBlack = function() {
         $scope.backBlack = !$scope.backBlack
         $scope.frontBlack = false;
     }
-    $scope.update = function(blackCard) {
-    	// $scope.blackCard.push();
-    	// $scope.blackCard = "";	
-    	//function call. this will call the flip function whenever update gets called
-    console.log('in the update',blackCard);
+    //black card preview button
+    $scope.update = function(blackCard, num) {
+    	console.log(blackCard, num);
+    	$scope.customBlack.question = blackCard;
+     	$scope.customBlack.blanks = num;
   	};
+  	//black card submit button
+  	$scope.createcustomBlack = function() {
+  		// No put values for $scope.customBlack in your cards database
+  		//yup
+    	console.log($scope.customBlack);
+  	};
+
   	$scope.togglefrontWhite = function() {
         $scope.frontWhite = !$scope.frontWhite
         $scope.backWhite = false;
       }
-      $scope.togglebackWhite = function() {
+    $scope.togglebackWhite = function() {
         $scope.backWhite = !$scope.backWhite
         $scope.frontWhite = false;
     }
+    //white card preview button
   	$scope.create = function(whiteCard) {
   		console.log(whiteCard);
-  		// $scope.whiteCard = "";
+  		$scope.customWhite.answer = whiteCard;
   	}; 
-
-});
+  	//white card submitt button
+  	$scope.createcustomWhite = function() {	
+  		console.log($scope.customWhite);
+  		};
+	});
 
