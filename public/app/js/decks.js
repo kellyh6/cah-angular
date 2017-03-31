ae3ce1a63d176cd12910967158a449dbfc238b37
dddotcom <dddotcom@gmail.com>
2017-03-31 10:19:37 -0700

    added some more deck stuff

--- public/app/js/decks.js
+++ public/app/js/decks.js
@@ -11,6 +11,23 @@ angular.module('DeckCtrls', ['Services'])
     console.log(err);
   });
 
+  BlackCardAPI.getCards().then(function success(response){
+    $scope.blackCards = response;
+    WhiteCardAPI.getCards().then(function success(response2){
+        $scope.whiteCards = response2;
+    }, function error(err){
+      console.log(err);
+    })
+  }, function error(err){
+    console.log(err);
+  });
+
+  //TODO: have all cards as an optio in the dropdown
+  //TODO: allow users to show cards from multiple decks
+  //TODO: when in the join view, allow users to select which decks they want to play the game with
+  //TODO: get a better shuffle function?
+  //TODO: show number of open rooms? How to close a room?
+
   $scope.getCards = function(){
     $scope.blackCards = [];
     $scope.whiteCards = [];
